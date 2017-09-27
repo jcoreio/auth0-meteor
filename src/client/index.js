21c19169676c33e4552bc218fbe6bcbbeb11ce5d
Andy Edwards <jedwards@fastmail.com>
2017-09-27 17:57:34 -0500

    fix(client/index.js): fix flow error

--- src/client/index.js
+++ src/client/index.js
@@ -7,7 +7,7 @@ import Lock from './Lock'
 if (Meteor.isClient) {
   Meteor.startup(() => {
     Meteor.call('getAuth0Attributes', function (error: ?Error, res?: Object) {
-      if (!res.AUTH0_CLIENTID || !res.AUTH0_DOMAIN) return
+      if (!res || !res.AUTH0_CLIENTID || !res.AUTH0_DOMAIN) return
       new Lock(res.AUTH0_CLIENTID, res.AUTH0_DOMAIN)
     })
   })
